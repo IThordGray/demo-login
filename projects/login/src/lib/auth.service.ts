95131b646f92c2c3b6d1f771d4da0ffdd2fed141
Ivor Thord-Gray <ivor.thordgray@gmail.com>
2023-06-04 11:44:57 +0200

    Initial commit

---
+++ projects/login/src/lib/auth.service.ts
@@ -0,0 +1,27 @@
+export interface IUser {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+export class InvalidCredentialsError extends Error {
+  constructor() {
+    super('Invalid username or password');
+  }
+}
+
+export abstract class AuthService {
+  accessToken?: string;
+  user?: IUser;
+
+  abstract loginAsync(username: string, password: string): Promise<void>;
+
+  abstract logoutAsync(): Promise<void>;
+
+  abstract registerAsync(firstName: string, lastName: string, username: string, password: string): Promise<void>;
+
+  abstract resetPasswordAsync(username: string): Promise<void>;
+
+  abstract setPasswordAsync(userId: string, resetCode: string, password: string): Promise<void>;
+}
---
+++ projects/login/src/lib/login.module.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { LOGIN_ROUTES } from './login.routes';
+import { LoginComponent } from './pages/login/login.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { SetPasswordComponent } from './pages/set-password/set-password.component';
+import { LoginContainerComponent } from './components/login-container/login-container.component';
+import { ResetPasswordSentComponent } from './pages/reset-password-sent/reset-password-sent.component';
+import { LogoComponent } from './components/logo/logo.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RouterLink,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule.forChild(LOGIN_ROUTES),
+    ReactiveFormsModule,
+    MatCheckboxModule
+  ],
+  declarations: [
+    LoginContainerComponent,
+    LoginComponent,
+    ResetPasswordComponent,
+    RegisterComponent,
+    SetPasswordComponent,
+    ResetPasswordSentComponent,
+    LogoComponent
+  ]
+})
+export class LoginModule {
+}
