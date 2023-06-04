2836bf42a214c623dfda3e1b1c045f8466699355
Ivor Thord-Gray <ivor.thordgray@gmail.com>
2023-06-04 12:09:52 +0200

    Add Nx to the workspace (#2)

---
+++ libs/login/src/lib/components/login-container/login-container.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'itg-login-container',
+  templateUrl: './login-container.component.html',
+  styleUrls: ['./login-container.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class LoginContainerComponent {}
